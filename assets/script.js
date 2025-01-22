dd9abc9b9e7474c964eb9fac5ee7f7040ee0ad62
joaootaviofm <joaootaviofm@gmail.com>
2025-01-22 14:44:30 -0300

    Javascript iniciado
    peguei variáveis de input e button e criei um objeto "romanToDec" para armazenar as conversões padronizadas

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var conversionBtn = document.getElementById('conversionButton')
+var inputRoman = document.getElementById('algTxt')
+
+const romanToDec = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'G': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000 
+}
+
